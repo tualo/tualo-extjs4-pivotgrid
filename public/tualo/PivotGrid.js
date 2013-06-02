0fda49618ec0ce7071b8ed56f40ee56189bf823b
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-06-02 09:31:53 +0200

    created matrix index, for store data

--- public/tualo/PivotGrid.js
+++ public/tualo/PivotGrid.js
@@ -262,20 +262,16 @@ Ext.define('Ext.tualo.PivotGrid', {
 				var val = leftAxisRange[lIndex].get('text');
 				var dIndex = leftAxisRange[lIndex].get('dataIndex');
 				fields.push({
-					//text: val,
 					name: dIndex,
 					type: 'string'
-					//_dataIndex: dIndex
 				});
 			}
 			
 			for(var i=0;i<cnf.length;i++){
 				if (typeof cnf[i].fromLeftAxis==='undefined'){
 					fields.push({
-						//text: cnf[i].text,
 						name: cnf[i].text,
-						type: 'number'
-						//_dataIndex: cnf[i].dataIndex
+						type: 'number' // must be the type or the Value-Field!
 					});
 				}
 			}
@@ -287,36 +283,38 @@ Ext.define('Ext.tualo.PivotGrid', {
 			});
 			
 			var storeData=this.getRowList(rows);
-			console.log(storeData);
-			// step down the rows,
-			/*
-			for(var i=0;i<rows.length;i++){
-				var items = this.getRowItems(rows[i],{});
-				//item[rows[i].dataIndex] = rows[i].text;
-				console.log(items);
-				
-				storeData.concat(items);
-			};
-			*/
-			
 			var store = Ext.create('Ext.data.Store',{
 				model: model,
 				data: storeData
 			});
-			//console.log(fields);
-			//console.log(rows);
-			//console.log(storeData);
-			
 			this.reconfigureStore(store);
-			
-			
-			
 			if (typeof myMask!=='undefined'){myMask.hide();}
+			
+			this.getMatrixIndex(storeData);
+			
 		}else{
 			myMask = new Ext.LoadMask(this.grid, {msg: this.waitText});
 			myMask.show();
 			Ext.defer(this.configureRows,10,this,[true,myMask]); // force Loading, delayed (the UI does not hang)
 		}
+	},
+	// create the index, by left and top axis
+	getMatrixIndex: function(rows){
+		if (typeof rows==='undefined') {rows = this.getRowList(this.getRows(0,[]));}
+		var fields={};
+		var columns = this.getColumnList(this.getColumns(0,[],false));
+		for(var rowIndex in rows){
+			var item = rows[rowIndex];
+			for(var colIndex in columns){
+				var filterObj = Ext.Object.merge(item,columns[colIndex]);
+				var id = Ext.JSON.encode(filterObj);
+				fields[id]={
+					col: colIndex,
+					row: rowIndex
+				};
+			}
+		} 
+		return fields;
 	},
 	getRowList: function(rows){
 		var elements = [];
@@ -333,6 +331,26 @@ Ext.define('Ext.tualo.PivotGrid', {
 			}else{
 				var element = {};
 				element[row.dataIndex]=row.text;
+				elements.push(element);
+			}
+		}
+		return elements;
+	},
+	getColumnList: function(columns){
+		var elements = [];
+		for(var i in columns){
+			var col = columns[i];
+			if (col.columns){
+				var subrows = this.getColumnList(col.columns);
+				for(var s in subrows){
+					var subrow = subrows[s];
+					var element = {};
+					element[col.dataIndex]=col.text;
+					elements.push(Ext.Object.merge(element,subrow));
+				}
+			}else{
+				var element = {};
+				element[col.dataIndex]=col.text;
 				elements.push(element);
 			}
 		}
@@ -356,30 +374,33 @@ Ext.define('Ext.tualo.PivotGrid', {
 		}
 		return rows;
 	},
-	getColumns: function(index,filter){
+	getColumns: function(index,filter,leftaxis){
+		if (typeof leftaxis==='undefined') {leftaxis=true;}
 		if (typeof index==='undefined') {index=0;}
 		if (typeof filter==='undefined') {filter=[];}
 		
 		var columns = [];
 		if (index==0){
-			// adding left Axis first
-			var leftAxisRange = (this.leftAxis.getRange());
-			for (var lIndex in leftAxisRange){
-				var val = leftAxisRange[lIndex].get('text');
-				var dIndex = leftAxisRange[lIndex].get('dataIndex');
-				columns.push({
-					text: val,
-					value: val,
-					dataIndex: dIndex,
-					fromLeftAxis: true // helper for calculations
-				})
+			if (leftaxis===true){
+				// adding left Axis first
+				var leftAxisRange = (this.leftAxis.getRange());
+				for (var lIndex in leftAxisRange){
+					var val = leftAxisRange[lIndex].get('text');
+					var dIndex = leftAxisRange[lIndex].get('dataIndex');
+					columns.push({
+						text: val,
+						value: val,
+						dataIndex: dIndex,
+						fromLeftAxis: true // helper for calculations
+					})
+				}
 			}
 		}
 		var axisRange = (this.topAxis.getRange());
 		if (axisRange.length>index){
 			columns = columns.concat(this.getDistinct(axisRange[index].get('dataIndex'),filter));
 			for(var i in columns){
-				var subColumns = this.getColumns(index+1,filter.concat(columns[i]));
+				var subColumns = this.getColumns(index+1,filter.concat(columns[i]),leftaxis);
 				if (subColumns.length>0){
 					columns[i].columns = subColumns;
 				}else{
